304792cc3d588c2881d474566800989ca384a61b
justgnoh <justingnoh@gmail.com>
2022-11-14 08:47:00 +0800

    Add JSON metadata file content update and renaming
    - Add Basic statistics after processing
    - Add Try-Catch to continue upon error
    -

--- file-rename.py
+++ file-rename.py
@@ -1,16 +1,57 @@
 import json
 import os
+import datetime
+
+countJSON = 0
+countProcessed = 0
+countFiles = 0
 
 # Read Json Files
 for file in os.listdir("./"):
+    countFiles += 1
+
     if file.endswith(".json"):
-        targetFile = open(file)
-        data = json.load(targetFile)
-        print(data["title"])
-    targetFile.close()
+        countJSON += 1
 
-# Find Said File
+        fileInProcessing = file
 
-# Rename Said File
+        try :
+            targetJSON = open(file)
+            dataJSON = json.load(targetJSON)
+            unix = dataJSON["photoTakenTime"]["timestamp"]
+            dt = datetime.datetime.utcfromtimestamp(float(unix)+28800) # 28,800 seconds is 8hours (+8GMT for SG)
 
-# Update JSON
+            fileName = os.path.splitext(dataJSON["title"])[0]
+            fileExtension = os.path.splitext(dataJSON["title"])[1]
+
+            targetJSON.close()
+
+            # Find Said File
+            if (os.path.exists(dataJSON["title"]) and os.path.isfile(dataJSON["title"])):
+                print("File Found... Processing: " + str(fileName))
+                countProcessed += 1
+
+                target = dt.strftime('%Y-%m-%d_%H%M%S') + "_" + fileName + fileExtension
+                
+                # Rename Said File
+                os.rename(dataJSON["title"], target)
+                
+                # Update JSON file
+                dataJSON["title"] = target
+                jsonFile = open(file, "w+")
+                jsonFile.seek(0)
+                jsonFile.write(json.dumps(dataJSON))
+                jsonFile.close()
+
+                # Rename JSON file
+                os.rename(fileName + fileExtension + ".json", target + ".json")
+                print("Successfully converted to: " + target)
+        except Exception as ex: 
+            print("===== Error: " + (fileInProcessing) + "=====")
+            print(ex)
+            continue
+
+print("countJSON:", countJSON)
+print("non JSON Files:", countFiles - countJSON)
+print("processed Files:",countProcessed)
+
